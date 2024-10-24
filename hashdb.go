2daf68372708fa8c85f6ec48504053d5d1805512
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-10-24 00:48:05 -0400

    Implement hashdb funcs

--- hashdb.go
+++ hashdb.go
@@ -1,1 +1,83 @@
 package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// path to hash
+var HASHDB_hash_table map[string][]byte
+
+// filename to path(s)
+var HASHDB_file_table map[string][]string
+
+var hash hash.Hash
+
+func hashdb_init() {
+	hash = sha256.New()
+}
+
+func hashdb_diff(path string, update bool) bool {
+	hash.Reset()
+
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return false
+	}
+
+	_, exists := HASHDB_hash_table[path]
+
+	newhash := hash.Sum(nil)
+	stored := make([]byte, 0)
+	if exists {
+		stored = HASHDB_hash_table[path]
+	}
+
+	if update {
+		HASHDB_hash_table[path] = hash.Sum(nil)
+		filename := filepath.Base(path)
+		_, ok := HASHDB_file_table[filename]
+		if !ok {
+			HASHDB_file_table[filename] = make([]string, 0)
+		}
+		HASHDB_file_table[filename] = append(HASHDB_file_table[filename], path)
+	}
+
+	if exists {
+		return !bytes.Equal(newhash, stored)
+	} else {
+		return true
+	}
+}
+
+func hashdb_update(path string) error {
+	hash.Reset()
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(hash, file)
+	if err != nil {
+		return err
+	}
+
+	HASHDB_hash_table[path] = hash.Sum(nil)
+	filename := filepath.Base(path)
+	_, ok := HASHDB_file_table[filename]
+	if !ok {
+		HASHDB_file_table[filename] = make([]string, 0)
+	}
+	HASHDB_file_table[filename] = append(HASHDB_file_table[filename], path)
+
+	return nil
+}
